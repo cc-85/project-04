82fbb95abb121ecaba8a140d6c22455ec5b828e1
Caoimhe Clarke <caoimhepower@gmail.com>
2018-10-23 16:35:51 +0100

    Initial commit

---
+++ config/routes.py
@@ -0,0 +1,14 @@
+import os
+from app import app
+
+# register your blueprints here...
+
+
+@app.route('/', defaults={'path': ''})  # homepage
+@app.route('/<path:path>')  # anything else eg: app.js /css/style.css
+def catch_all(path):
+    if os.path.isfile('public/' + path):  # if path is a file
+        return app.send_static_file(path)  # send back the file
+
+    # otherwise send back index.html
+    return app.send_static_file('index.html')
