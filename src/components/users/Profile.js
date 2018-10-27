88e58ccb4790e0a296d78f0c6f5085eb4d6bc096
James Robins <jamesrobins101@hotmail.com>
2018-10-27 16:35:40 +0100

    Created User page

--- src/components/users/Profile.js
+++ src/components/users/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import axios from 'axios';
+import Auth from '../../lib/Auth';
 
 
 class Profile extends React.Component {
@@ -8,11 +9,45 @@ class Profile extends React.Component {
     this.state = { cocktails: []};
   }
 
+  componentDidMount() {
+    const token = Auth.getToken();
+    axios.get(`/api/users/${Auth.getPayload().sub}`, {
+      headers: {Authorization: `Bearer ${token}`}
+    })
+      .then(res => this.setState({ user: res.data }));
 
+  }
 
   render() {
+    if(!this.state.user) return null;
     return (
-      <h1 className="title is-1">User Profile Page</h1>
+      <main className="section">
+        <div className="container">
+
+          <h1 className="title is-1">User Profile Page</h1>
+
+          <figure className="image profile-picture is-128x128">
+            <img src={ this.state.user.profile_image } />
+          </figure>
+
+          <h4 className="title is-4">Username:</h4>
+          <p>Username: {  this.state.user.username }</p>
+
+          <h4 className="title is-4">Email:</h4>
+          <p>Email: {  this.state.user.email }</p>
+
+          <h4 className="title is-4">Image link:</h4>
+          <p>Profile Image: {  this.state.user.profile_image }</p>
+
+          <h4 className="title is-4">Ingredients</h4>
+          <ul>
+            { this.state.user.ingredients ? this.state.user.ingredients.map(ingredient =>
+              <li key={ingredient.id}>{ ingredient.name }</li>
+            ) : <p>Add ingredients</p>}
+          </ul>
+
+        </div>
+      </main>
     );
   }
 }
