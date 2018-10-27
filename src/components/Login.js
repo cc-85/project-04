93f93742699635030861518c5fb396a253636814
James Robins <jamesrobins101@hotmail.com>
2018-10-27 11:29:45 +0100

    Created Login and Register pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,30 @@
 import React from 'react';
-
-
+import axios from 'axios';
+import Auth from '../lib/Auth';
 
 class Login extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = { credentials: null };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    const credentials = { ...this.state.credentials, [e.target.name]: e.target.value };
+    this.setState({ credentials, error: '' });
   }
 
   handleSubmit(e) {
     e.preventDefault();
+    axios
+      .post('/api/login', this.state.credentials)
+      .then( res => {
+        Auth.setToken(res.data.token);
 
+        this.props.history.push('/');
+      })
+      .catch(() => this.setState({error: 'Invalid credentials'}));
   }
 
   render() {
@@ -33,6 +47,7 @@ class Login extends React.Component {
                 <input className={`input ${this.state.error ? 'is-danger' : ''} `} name="password" placeholder="Password" onChange={this.handleChange} />
               </div>
             </div>
+            {this.state.error && <small className="help is-danger">{this.state.error}</small>}
 
             <button className="button is-primary">Submit</button>
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,31 @@
 import React from 'react';
-
+import axios from 'axios';
+import Auth from '../lib/Auth';
 
 
 class Register extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = { credentials: null };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(e) {
+    const credentials = { ...this.state.credentials, [e.target.name]: e.target.value };
+    this.setState({ credentials, error: '' });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .post('/api/register', this.state.credentials)
+      .then( res => {
+        Auth.setToken(res.data.token);
+        this.props.history.push('/');
+      })
+      .catch(() => this.setState({error: 'Invalid credentials'}));
+  }
 
   render() {
     return (
@@ -42,9 +60,9 @@ class Register extends React.Component {
               </div>
             </div>
             <div className="field">
-              <label className="label">Password Confirmation</label>
+              <label className="label">Confirmation Password</label>
               <div className="control">
-                <input className={`input ${this.state.error ? 'is-danger' : ''} `} name="passwordConfirmation" type="password" placeholder="Password" onChange={this.handleChange} />
+                <input className={`input ${this.state.error ? 'is-danger' : ''} `} name="password_confirmation" type="password" placeholder="Password" onChange={this.handleChange} />
               </div>
             </div>
             {this.state.error && <small className="help is-danger">{this.state.error}</small>}
