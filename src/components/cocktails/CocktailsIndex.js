9b1a57d6ce2099d655e5f7e9bce40fb7b8aed9f6
Caoimhe Clarke <caoimhepower@gmail.com>
2018-10-28 17:09:55 +0000

    Search bar for cocktail names

--- src/components/cocktails/CocktailsIndex.js
+++ src/components/cocktails/CocktailsIndex.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-
+import FilterBar from '../FilterBar';
+import _ from 'lodash';
 
 
 class CocktailsIndex extends React.Component {
   constructor() {
     super();
-    this.state = { cocktails: []};
+    this.state = { cocktails: [], filter: '' };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -16,7 +18,22 @@ class CocktailsIndex extends React.Component {
       .then(res => this.setState({ cocktails: res.data}));
   }
 
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
+  //creates a sorted array for us to loop over
+  getFilteredCocktails() {
+    const re = new RegExp(this.state.filter, 'i');
+    return _.filter(this.state.cocktails, cocktail => re.test(cocktail.name));
+    // || cocktail.ingredients.some(re.test(name)));
+  }
+
+  //creates a filtered array for us to loop over
+  // filterCountries() {
+  //   const re = new RegExp(this.state.filter, 'i');
+  //   return _.filter(this.state.countries, country => re.test(country.name));
+  // }
 
   render() {
     console.log(this.state);
@@ -24,9 +41,9 @@ class CocktailsIndex extends React.Component {
       <main className="section">
         <div className="container">
           <h1 className="title is-1">Cocktails Index page</h1>
-
+          <FilterBar handleChange={this.handleChange} />
           <div className="columns is-multiline">
-            {this.state.cocktails.map(cocktail =>
+            {this.getFilteredCocktails().map(cocktail =>
               <div key={cocktail.id} className="column is-one-quarter">
                 <Link to={`/cocktails/${cocktail.id}`}>
                   <div className="card">
