ec468855a4be8003a647b6e78cba869e7e66236b
Caoimhe Clarke <caoimhepower@gmail.com>
2018-10-28 14:15:27 +0000

    Navbar changes when user is logged in and out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
-
+import Auth from '../lib/Auth';
+import axios from 'axios';
 
 class Navbar extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { user: null };
+    this.logout = this.logout.bind(this);
 
+  }
+
+  getCurrentUser() {
+    const token = Auth.getToken();
+    axios.get(`/api/users/${Auth.getPayload().sub}`, {
+      headers: { Authorization: `Bearer ${token}`}
+    })
+      .then(res => this.setState({ user: res.data }));
+  }
+
+  logout() {
+    Auth.logout();
+    this.props.history.push('/');
   }
 
 
@@ -37,10 +52,10 @@ class Navbar extends React.Component {
           <div className="navbar-menu">
             <div className="navbar-end">
               <Link className="navbar-item" to="/cocktails"> All cocktails</Link>
-              <a className="navbar-item" onClick={this.logout}>Logout</a>
-              <Link href="#" className="navbar-item nav-icon" to="/profile">Profile</Link>
-              <Link href="#" className="navbar-item" to="/login"> Login </Link>
-              <Link href="#" className="navbar-item" to="/register"> Register</Link>
+              {Auth.isAuthenticated() && <a className="navbar-item" onClick={this.logout}>Logout</a>}
+              {Auth.isAuthenticated() && <Link href="#" className="navbar-item nav-icon" to="/profile">Profile</Link>}
+              {!Auth.isAuthenticated() && <Link href="#" className="navbar-item" to="/login"> Login </Link>}
+              {!Auth.isAuthenticated() && <Link href="#" className="navbar-item" to="/register"> Register</Link>}
             </div>
           </div>
         </div>
