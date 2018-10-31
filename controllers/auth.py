b45cbf1030a44a019ec83c96bf4cb47f7c652856
James Robins <jamesrobins101@hotmail.com>
2018-10-31 21:06:09 +0000

    make create_token function, token issued at register

--- controllers/auth.py
+++ controllers/auth.py
@@ -10,29 +10,7 @@ user_schema = UserSchema()
 api = Blueprint('auth', __name__)
 
 
-@api.route('/register', methods=['POST'])
-def register():
-    try:
-        data = user_schema.load(request.get_json())
-    except ValidationError as error:
-        return jsonify({'error': error.messages}), 422
-
-    user = User(data)
-    user.save()
-
-    return jsonify({'message': 'Registation successful'})
-
-
-@api.route('/login', methods=['POST'])
-def login():
-
-    data = request.get_json()
-
-    user = User.query.filter_by(email=data.get('email')).first()
-
-    if not user or not user.validate_password(data.get('password', '')):
-        return jsonify({'message': 'Unauthorized'}), 401
-
+def create_token(user):
     payload = {
         'exp': datetime.utcnow() + timedelta(days=1),
         'iat': datetime.utcnow(),
@@ -45,7 +23,36 @@ def login():
         'HS256'
     ).decode('utf-8')
 
+    return token
+
+
+@api.route('/register', methods=['POST'])
+def register():
+    try:
+        data = user_schema.load(request.get_json())
+    except ValidationError as error:
+        return jsonify({'error': error.messages}), 422
+
+    user = User(data)
+    user.save()
+
+    return jsonify({
+        'message': 'Registation successful',
+        'token': create_token(user)
+    })
+
+
+@api.route('/login', methods=['POST'])
+def login():
+
+    data = request.get_json()
+
+    user = User.query.filter_by(email=data.get('email')).first()
+
+    if not user or not user.validate_password(data.get('password', '')):
+        return jsonify({'message': 'Unauthorized'}), 401
+
     return jsonify({
         'message': 'Welcome back {}!'.format(user.username),
-        'token': token
+        'token': create_token(user)
     })
